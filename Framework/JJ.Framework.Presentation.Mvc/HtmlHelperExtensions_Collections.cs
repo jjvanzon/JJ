8cd3551eab4f2bd1f627211b933b828e3ae079bf
jjvanzon <janjoostvanzon@hotmail.com>
2014-01-07 21:56:48 +0000

    QuestionDetailView: You can edit everything but the categories, validations are shown in a list, but not near the corresponding field; Presentation.JavaScript: remember scroll position on refresh; WCF app service still temporarily does not work now because presenters return object instead of a specific view model;

---
+++ Framework/JJ.Framework.Presentation.Mvc/HtmlHelperExtensions_Collections.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace JJ.Framework.Presentation.Mvc
+{
+    public static class HtmlHelperExtensions_Collections
+    {
+        public static IDisposable BeginItem(this HtmlHelper htmlHelper, Expression<Func<object>> expression)
+        {
+            Qualifier qualifier = GetCurrentQualifier(htmlHelper);
+
+            string identifier = ExpressionHelper.GetExpressionText(expression);
+
+            qualifier.AddItemNode(htmlHelper, identifier);
+
+            return qualifier;
+        }
+
+        public static IDisposable BeginCollection(this HtmlHelper htmlHelper, Expression<Func<object>> expression)
+        {
+            Qualifier qualifier = GetCurrentQualifier(htmlHelper);
+
+            string identifier = ExpressionHelper.GetExpressionText(expression);
+
+            qualifier.AddCollectionNode(htmlHelper, identifier);
+
+            return qualifier;
+        }
+
+        // TODO: You need an overload that takes an int index for when the structure in the viewmodel maps to a totally different structure in the view.
+
+        public static IDisposable BeginCollectionItem(this HtmlHelper htmlHelper)
+        {
+            Qualifier qualifier = GetCurrentQualifier(htmlHelper);
+
+            qualifier.IncrementIndex(htmlHelper);
+
+            return new DummyDisposable(); 
+        }
+
+        private static object _qualifierDictionaryLock = new object();
+
+        private static Dictionary<object, Qualifier> _qualifierDictionary = new Dictionary<object, Qualifier>();
+
+        private static Qualifier GetCurrentQualifier(HtmlHelper htmlHelper)
+        {
+            lock (_qualifierDictionaryLock)
+            {
+                object key = Thread.CurrentThread.ManagedThreadId;
+
+                if (!_qualifierDictionary.ContainsKey(key))
+                {
+                    _qualifierDictionary[key] = new Qualifier(htmlHelper);
+                }
+
+                return _qualifierDictionary[key];
+            }
+        }
+
+        /// <summary>
+        /// A qualifier to which you can add item nodes, collection nodes and indexes.
+        /// Automatically assigns the HtmlFieldPrefix and adds hidden input elements that store the indexes.
+        /// When Dispose is called, the last node of the qualifier is removed.
+        /// If no nodes are left, Dispose restores the original HtmlFieldPrefix.
+        /// </summary>
+        private class Qualifier : IDisposable
+        {
+            private readonly HtmlHelper _originalHtmlHelper;
+            private readonly string _originalHtmlFieldPrefix;
+            private readonly Stack<Node> _nodes = new Stack<Node>();
+
+            public Qualifier(HtmlHelper htmlHelper)
+            {
+                _originalHtmlHelper = htmlHelper;
+                _originalHtmlFieldPrefix = _originalHtmlHelper.ViewData.TemplateInfo.HtmlFieldPrefix;
+            }
+
+            public void AddItemNode(HtmlHelper htmlHelper, string identifier)
+            {
+                _nodes.Push(new ItemNode(identifier));
+
+                htmlHelper.ViewData.TemplateInfo.HtmlFieldPrefix = FormatText();
+            }
+
+            public void AddCollectionNode(HtmlHelper htmlHelper, string identifier)
+            {
+                _nodes.Push(new CollectionNode(identifier));
+            }
+
+            public void IncrementIndex(HtmlHelper htmlHelper)
+            {
+                // You cannot use the HtmlHelper from the constructor,
+                // because the HtmlHelper can change when you go from one partial view to another.
+
+                var node = _nodes.Peek() as CollectionNode;
+                if (node == null)
+                {
+                    throw new Exception("Cannot call BeginCollectionItem, because you are not inside a BeginCollection scope.");
+                }
+
+                node.IncrementIndex();
+
+                WriteHiddenIndexField(htmlHelper, node.Index);
+
+                htmlHelper.ViewData.TemplateInfo.HtmlFieldPrefix = FormatText();
+            }
+
+            private void WriteHiddenIndexField(HtmlHelper htmlHelper, int index)
+            {
+                // When indexes of collections are not consecutive, they do not bind it to the model. Unless you add a '{0}.index' hidden field.
+
+                TextWriter writer = htmlHelper.ViewContext.Writer;
+
+                string formattedText = FormatText();
+                string collectionName = CutOffIndex(formattedText);
+                string indexFieldName = String.Format("{0}.index", collectionName);
+
+                // Tip from original BeginCollectionItem author:
+                // "    autocomplete="off" is needed to work around a very annoying Chrome behaviour
+                //      whereby it reuses old values after the user clicks "Back", which causes the
+                //      xyz.index and xyz[...] values to get out of sync.   "
+
+                // You cannot use HtmlHelper.Hidden, because it will use the wrong name prefix, and also generate a bad ID field with the wrong prefix.
+                string html = String.Format(@"<input type=""hidden"" name=""{0}"" value=""{1}"" autocomplete=""off""/>", htmlHelper.Encode(indexFieldName), htmlHelper.Encode(index));
+
+                writer.Write(html);
+            }
+
+            private string CutOffIndex(string input)
+            {
+                int pos = input.LastIndexOf('[');
+                string output = input.Substring(0, pos);
+                return output;
+            }
+
+            public string FormatText()
+            {
+                return String.Join(".", _nodes.Select(x => x.FormatText()).Reverse());
+            }
+
+            public void Dispose()
+            {
+                if (_nodes.Count > 0)
+                {
+                    _nodes.Pop();
+                }
+                else
+                {
+                    _originalHtmlHelper.ViewData.TemplateInfo.HtmlFieldPrefix = _originalHtmlFieldPrefix;
+                }
+            }
+        }
+
+        private abstract class Node
+        {
+            public string Identifier { get; private set; }
+
+            public Node(string identifier)
+            {
+                Identifier = identifier;
+            }
+
+            public abstract string FormatText();
+        }
+
+        private class ItemNode : Node
+        {
+            public ItemNode(string identifier)
+                : base(identifier)
+            { }
+
+            public override string FormatText()
+            {
+                return Identifier;
+            }
+        }
+
+        private class CollectionNode : Node
+        {
+            public CollectionNode(string identifier)
+                : base(identifier)
+            {
+                Index = -1;
+            }
+
+            public int Index { get; private set; }
+
+            public void IncrementIndex()
+            {
+                Index++;
+            }
+
+            public override string FormatText()
+            {
+                return Identifier + "[" + Index.ToString() + "]";
+            }
+        }
+
+        private class DummyDisposable : IDisposable
+        {
+            public void Dispose()
+            { }
+        }
+    }
+}
---
+++ Framework/JJ.Framework.Presentation.Mvc/UrlHelpers.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JJ.Framework.Presentation.Mvc
+{
+    public static class UrlHelpers // Plural not to conflict with the name 'UrlHelper'.
+    {
+        public static string GetUrlWithCollectionParameter<T>(string actionName, string controllerName, string parameterName, IEnumerable<T> collection)
+        {
+            // First URL-encode everything.
+            actionName = HttpUtility.UrlEncode(actionName);
+            controllerName = HttpUtility.UrlEncode(controllerName);
+            parameterName = HttpUtility.UrlEncode(parameterName);
+
+            var values = new List<string>();
+            foreach (var x in collection)
+            {
+                string str = Convert.ToString(x);
+                string value = HttpUtility.UrlEncode(str);
+                values.Add(value);
+            }
+
+            // Build the URL parameter string.
+            string parameterString = "";
+            if (collection.Count() != 0)
+            {
+                parameterString = "?" + parameterName + "=" + String.Join("&" + parameterName + "=", values);
+            }
+
+            // Build the URL.
+            string url = "/" + controllerName + "/" + actionName + parameterString;
+
+            return url;
+        }
+
+        public static string GetUrl(string actionName, string controllerName, params object[] parameterNamesAndValues)
+        {
+            // First HTML-encode all elements of the url, for safety.
+            actionName = HttpUtility.HtmlEncode(actionName);
+            controllerName = HttpUtility.HtmlEncode(controllerName);
+
+            // Build the URL parameter string.
+            string parametersString = "";
+            ICollection<KeyValuePair<string, object>> parameters = ConvertNamesAndValuesListToKeyValuePairs(parameterNamesAndValues);
+            if (parameters != null && parameters.Count != 0)
+            {
+                var list = new List<string>();
+                foreach (var entry in parameters)
+                {
+                    string name = HttpUtility.UrlEncode(entry.Key);
+                    string value = HttpUtility.UrlEncode(Convert.ToString(entry.Value));
+                    string str = name + "=" + value;
+                    list.Add(str);
+                }
+
+                parametersString = "?" + String.Join("&", list);
+            }
+
+            // Build the URL.
+            string url = "/" + controllerName + "/" + actionName + parametersString;
+
+            return url;
+        }
+
+        private static ICollection<KeyValuePair<string, object>> ConvertNamesAndValuesListToKeyValuePairs(IList<object> namesAndValues)
+        {
+            // Allow converting null to null.
+            if (namesAndValues == null) 
+            {
+                return null; 
+            }
+
+            if (namesAndValues.Count % 2 != 0) { throw new Exception("namesAndValues.Count must be a multiple of 2."); }
+
+            var keyValuePairs = new List<KeyValuePair<string, object>>();
+
+            for (int i = 0; i < namesAndValues.Count; i += 2)
+            {
+                object name = namesAndValues[i];
+                object value = namesAndValues[i + 1];
+
+                if (name == null) { throw new Exception("Names in namesAndValues cannot contain nulls."); }
+                if (name.GetType() != typeof(string)) { throw new Exception("Names in namesAndValues must be strings."); }
+
+                keyValuePairs.Add(new KeyValuePair<string,object>((string)name, value));
+            }
+
+            return keyValuePairs;
+        }
+
+        /*public static string GetUrl(string actionName, string controllerName, ICollection<KeyValuePair<string, object>> parameters = null)
+        {
+            // First HTML-encode all elements of the url, for safety.
+            actionName = HttpUtility.HtmlEncode(actionName);
+            controllerName = HttpUtility.HtmlEncode(controllerName);
+
+            // Build the URL parameter string.
+            string parametersString = "";
+            if (parameters != null && parameters.Count != 0)
+            {
+                var list = new List<string>();
+                foreach (var entry in parameters)
+                {
+                    string name = HttpUtility.UrlEncode(entry.Key);
+                    string value =  HttpUtility.UrlEncode(Convert.ToString(entry.Value));
+                    string str = name + "=" + value;
+                    list.Add(str);
+                }
+
+                parametersString = "?" + String.Join("&", list);
+            }
+
+            // Build the URL.
+            string url = "/" + controllerName + "/" + actionName + parametersString;
+
+            return url;
+        }*/
+    }
+}
