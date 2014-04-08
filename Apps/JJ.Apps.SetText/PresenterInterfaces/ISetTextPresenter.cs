4393eb55734357f621e778931b74014bd0b70dbe
jjvanzon <janjoostvanzon@hotmail.com>
2014-04-08 21:01:25 +0000

    SetText Online/offline variations in WinForms

---
+++ Apps/JJ.Apps.SetText/PresenterInterfaces/ISetTextPresenter.cs
@@ -0,0 +1,19 @@
+﻿using JJ.Apps.SetText.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace JJ.Apps.SetText.PresenterInterfaces
+{
+    [ServiceContract]
+    public interface ISetTextPresenter
+    {
+        [OperationContract]
+        SetTextViewModel Show();
+
+        [OperationContract]
+        SetTextViewModel Save(SetTextViewModel viewModel);
+    }
+}
---
+++ Apps/JJ.Apps.SetText/PresenterInterfaces/ISetTextWithSyncPresenter.cs
@@ -0,0 +1,22 @@
+﻿using JJ.Apps.SetText.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace JJ.Apps.SetText.PresenterInterfaces
+{
+    [ServiceContract]
+    public interface ISetTextWithSyncPresenter
+    {
+        [OperationContract]
+        SetTextWithSyncViewModel Show();
+
+        [OperationContract]
+        SetTextWithSyncViewModel Save(SetTextWithSyncViewModel viewModel);
+
+        [OperationContract]
+        SetTextWithSyncViewModel Synchronize(SetTextWithSyncViewModel viewModel);
+    }
+}
