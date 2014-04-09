f98af3c6c75966b1340546fbc7554bc665e7def1
jjvanzon <janjoostvanzon@hotmail.com>
2014-04-09 21:36:55 +0000

    SetText online/offline variations in Unity

--- Models/JJ.Models.Canonical/ValidationMessage.cs
+++ Models/JJ.Models.Canonical/ValidationMessage.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace JJ.Models.Canonical
 {
+    [DataContract]
     public class ValidationMessage
     {
+        [DataMember]
         public string PropertyKey { get; set; }
+
+        [DataMember]
         public string Text { get; set; }
     }
 }
