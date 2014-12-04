a967c518f0618760b46a4267e6d017dd2f8183f0
jjvanzon <janjoostvanzon@hotmail.com>
2014-12-04 20:49:47 +0000

    TryUnityWww

---
+++ Apps/JJ.Apps.QuestionAndAnswer/Extensions/QuestionLinkExtensions_ToViewModel.cs
@@ -0,0 +1,26 @@
+﻿using JJ.Apps.QuestionAndAnswer.ViewModels;
+using JJ.Models.QuestionAndAnswer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Apps.QuestionAndAnswer.Extensions
+{
+    internal static class QuestionLinkExtensions_ToViewModel
+    {
+        public static QuestionLinkViewModel ToViewModel(this QuestionLink entity)
+        {
+            if (entity == null) { throw new ArgumentNullException("entity"); }
+
+            return new QuestionLinkViewModel
+            {
+                ID = entity.ID,
+                TemporaryID = Guid.NewGuid(),
+                Description = entity.Description,
+                Url = entity.Url
+            };
+        }
+    }
+}
---
+++ Apps/JJ.Apps.QuestionAndAnswer/Extensions/QuestionTypeExtensions_ToViewModel.cs
@@ -0,0 +1,24 @@
+﻿using JJ.Apps.QuestionAndAnswer.ViewModels;
+using JJ.Models.QuestionAndAnswer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Apps.QuestionAndAnswer.Extensions
+{
+    internal static class QuestionTypeExtensions_ToViewModel
+    {
+        public static QuestionTypeViewModel ToViewModel(this QuestionType entity)
+        {
+            if (entity == null) { throw new ArgumentNullException("entity"); }
+
+            return new QuestionTypeViewModel 
+            {
+                ID = entity.ID,
+                Name = entity.Name
+            };
+        }
+    }
+}
