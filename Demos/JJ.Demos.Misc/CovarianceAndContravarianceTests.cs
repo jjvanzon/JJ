028e981ed20ac4d9cea75ceae741044afd1d2b4a
jjvanzon <janjoostvanzon@hotmail.com>
2015-02-28 20:40:04 +0000

    Generalized ActionDispatcher for MVC works. Generalization of ActionDispatcher for Presenters not finished yet. Accessor class ported from Circle code base to JJ code base.

---
+++ Demos/JJ.Demos.Misc/CovarianceAndContravarianceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Demos.Misc
+{
+    [TestClass]
+    public class CovarianceAndContravarianceTests
+    {
+        private class Base
+        { }
+
+        private class DerivedClass : Base
+        { }
+
+        [TestMethod]
+        private void Test_ContraVariance_Func()
+        {
+            Func<object, object> baseFunc = b => b;
+            Func<DerivedClass, object> derivedFunc = d => d;
+
+            //baseFunc = derivedFunc; // Does not compile.
+            derivedFunc = baseFunc;
+        }
+    }
+}
