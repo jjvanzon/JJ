032ed8338179ed66dbe96577bffeb2a370041202
jjvanzon <janjoostvanzon@hotmail.com>
2015-03-19 18:44:53 +0000

    Svg: first try setting up an Svg model. Before switching from using persistence/business layers to implementing implementing it as a presentation API.

---
+++ Business/JJ.Business.Svg/Gestures/ClickGesture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Business.Svg.Gestures
+{
+    public class ClickGesture : IGesture
+    {
+        public void MouseDown(Infos.MouseDownInfo info)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MouseMove(Infos.MouseDownInfo info)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MouseUp(Infos.MouseDownInfo info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
