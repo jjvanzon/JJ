3d61a7537cfd1203652f9dde6f0255838b374d75
jjvanzon <janjoostvanzon@hotmail.com>
2015-09-04 02:27:13 +0200

    Synthesizer: Remove unused namespaces solution-wide (except for external components)

--- Business/JJ.Business.CanonicalModel/IDNameAndObjectType.cs
+++ Business/JJ.Business.CanonicalModel/IDNameAndObjectType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace JJ.Business.CanonicalModel
+﻿namespace JJ.Business.CanonicalModel
 {
     /// <summary>
     /// TODO: Remove class if no longer used.
