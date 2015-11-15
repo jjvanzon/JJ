c8098fe5f31d7efcdb640398d93613175fa5a00e
jjvanzon <janjoostvanzon@hotmail.com>
2015-11-15 20:11:59 +0100

    Synthesizer: Generate names unique within document including child documents + some loose TODO's.

--- Demos/JJ.Demos.Misc/MiscTests.cs
+++ Demos/JJ.Demos.Misc/MiscTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using JJ.Framework.Testing;
+using System.Globalization;
 
 namespace JJ.Demos.Misc
 {
@@ -15,6 +16,21 @@ namespace JJ.Demos.Misc
             IList<int> source = new int[] { 1, 2, 3 };
             IList<int> dest = source.Take(5).ToArray();
             AssertHelper.AreEqual(3, () => dest.Count);
+        }
+
+        [TestMethod]
+        public void Test_CultureInfo_TextInfo_ListSeparator_Chinese_Etc()
+        {
+            CultureInfo chineseCulture = new CultureInfo("zh-CN");
+            CultureInfo russianCulture = new CultureInfo("ru-RU");
+            CultureInfo dutchCulture = new CultureInfo("nl-NL");
+            CultureInfo enUSCulture = new CultureInfo("en-US");
+
+            // These are not exactly what you would expect in terms of punctuation, so in most cases there is no point using it.
+            string chineseListSeparator = chineseCulture.TextInfo.ListSeparator;
+            string russianListSeparator = russianCulture.TextInfo.ListSeparator;
+            string dutchListSeparator = dutchCulture.TextInfo.ListSeparator;
+            string enUSListSeparator = enUSCulture.TextInfo.ListSeparator;
         }
     }
 }
