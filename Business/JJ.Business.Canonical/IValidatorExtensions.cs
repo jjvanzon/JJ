5c08c0322356ce2fa894058e1fbc161ff07d172e
jjvanzon <janjoostvanzon@hotmail.com>
2016-07-11 22:03:52 +0200

    Synthesizer: Refactoring: PatchManager side-effects and validations less conditional and more self-sufficient.

---
+++ Business/JJ.Business.Canonical/IValidatorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JJ.Data.Canonical;
+using JJ.Framework.Reflection.Exceptions;
+using JJ.Framework.Validation;
+
+namespace JJ.Business.Canonical
+{
+    public static class IValidatorExtensions
+    {
+        public static VoidResult ToResult(this IValidator validator)
+        {
+            if (validator == null) throw new NullException(() => validator);
+
+            var result = new VoidResult
+            {
+                Successful = false,
+                Messages = validator.ValidationMessages.ToCanonical()
+            };
+
+            return result;
+        }
+    }
+}
