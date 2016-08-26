db9167523d6aa9411152a707408d77f5d92c9cb3
jjvanzon <janjoostvanzon@hotmail.com>
2016-08-27 01:11:06 +0200

    Synthesizer: Data: Remove Document.GroupName, Document.ParentDocument and Document.ChildDocuments

--- Data/JJ.Data.Canonical/IDAndName.cs
+++ Data/JJ.Data.Canonical/IDAndName.cs
@@ -1,8 +1,14 @@
-﻿namespace JJ.Data.Canonical
+﻿using System.Runtime.Serialization;
+
+namespace JJ.Data.Canonical
 {
+    [DataContract]
     public class IDAndName
     {
+        [DataMember]
         public int ID { get; set; }
+
+        [DataMember]
         public string Name { get; set; }
     }
 }
