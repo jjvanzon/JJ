c5bece7f5783242b9a05531605494cd80611f69e
jjvanzon <janjoostvanzon@hotmail.com>
2017-06-12 00:50:01 +0200

    Synthesizer: Bootstrapped the 'And' Operator.

--- Business/Canonical/ResultExtensions.cs
+++ Business/Canonical/ResultExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using JJ.Framework.Collections;
 using JJ.Framework.Exceptions;
 using JJ.Data.Canonical;
@@ -31,13 +32,21 @@ namespace JJ.Business.Canonical
 
         // Business
 
-        public static void Combine(this IResult destResult, IResult sourceResult)
+        public static void Combine(this IResult destResult, IResult sourceResult, string messagePrefix)
         {
             if (destResult == null) throw new NullException(() => destResult);
             if (sourceResult == null) throw new NullException(() => sourceResult);
 
             destResult.Successful &= sourceResult.Successful;
-            destResult.Messages.AddRange(sourceResult.Messages);
+
+            if (!string.IsNullOrEmpty(messagePrefix))
+            {
+                destResult.Messages.AddRange(sourceResult.Messages.Select(x => new Message(x.Key, messagePrefix + x.Text)));
+            }
+            else
+            {
+                destResult.Messages.AddRange(sourceResult.Messages);
+            }
         }
 
         public static void Assert(this IResult result)
