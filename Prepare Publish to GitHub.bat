8c88a93387432e647b0fd8320d8d6b7b38304632
jjvanzon <janjoostvanzon@hotmail.com>
2019-08-03 20:53:41 +0200

    JJ.Business.Canonical: Minor refactorings. Prepare Publish to GitHub.bat: Add "JJ\Demos\Git Scripts to Isolate Project from Larger Repository" to the copy scripting.

--- Prepare Publish to GitHub.bat
+++ Prepare Publish to GitHub.bat
@@ -35,6 +35,7 @@ xcopy /s /v /r /q "JJ\Demos\IndexersProposal" "JJ.SaveText on GitHub\Demos\Index
 xcopy /s /v /r /q "JJ\Demos\JavaScript" "JJ.SaveText on GitHub\Demos\JavaScript\"
 xcopy /s /v /r /q "JJ\Demos\Misc" "JJ.SaveText on GitHub\Demos\Misc\"
 xcopy /s /v /r /q "JJ\Demos\SvnAndTfsToGitMigrationScripts" "JJ.SaveText on GitHub\Demos\SvnAndTfsToGitMigrationScripts\"
+xcopy /s /v /r /q "JJ\Demos\Git Scripts to Isolate Project from Larger Repository" "JJ.SaveText on GitHub\Demos\Git Scripts to Isolate Project from Larger Repository\"
 xcopy /s /v /r /q "JJ\Demos\TryUnityWww" "JJ.SaveText on GitHub\Demos\TryUnityWww\"
 xcopy /v /r /q "JJ\Demos\2013-01-11 Recursion.cs" "JJ.SaveText on GitHub\Demos"
 mkdir "JJ.SaveText on GitHub\Business\"
