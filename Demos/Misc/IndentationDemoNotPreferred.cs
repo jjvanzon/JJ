b58fa746fbe17e9155cc56653b0e34ce6285e182
jjvanzon <janjoostvanzon@hotmail.com>
2023-01-03 20:18:27 +0100

    ➡ IndentationDemo

---
+++ Demos/Misc/IndentationDemoNotPreferred.cs
@@ -0,0 +1,50 @@
+﻿using JJ.Framework.Reflection;
+using System;
+// ReSharper disable UnusedType.Global
+// ReSharper disable ArrangeTypeMemberModifiers
+// ReSharper disable ArrangeTypeModifiers
+// ReSharper disable UnusedMember.Local
+// ReSharper disable InvertIf
+
+namespace Demos.Misc
+{
+class IndentationDemoNotPreferred
+{
+        object ParseValue(string input,
+Type type 
+            )
+{
+        if (type.IsNullableType()) 
+        {
+    if (string.IsNullOrEmpty(input)) {
+    return null;
+}
+
+type = type.GetUnderlyingNullableTypeFast();
+    }
+
+        if (type.IsEnum) 
+    {
+return Enum.Parse(type, input);
+    }
+
+            if (type == 
+    typeof(TimeSpan))
+{
+                return TimeSpan.Parse(input);
+}
+
+            if (type == typeof(Guid))
+            { 
+    return new Guid(input);
+            }
+
+
+        return 
+        Convert.ChangeType(
+   input, 
+
+        type);
+                }
+                }
+            }
