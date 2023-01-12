1a89815012e8c0889324b3a865d31db10e97773d
jjvanzon <janjoostvanzon@hotmail.com>
2023-01-12 21:21:58 +0100

    💎 ValueAndHasValueDemo

---
+++ Demos/Misc/ValueAndHasValueDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Demos.Misc
+{
+    internal class ValueAndHasValueDemo
+    {
+        int? number;
+
+        private void Recommended()
+        {
+            if (number.HasValue)
+            {
+                string message = $"Number = {number.Value}";
+            }
+        }
+
+        private void LessPreferred()
+        {
+            if (number != null)
+            {
+                string message = $"Number = {number}");
+            }
+        }
+
+    }
+}
